d2cf375bebec43f4b10bfe4d5b733ef4f86dba87
LightGuard <lightguard.jp@gmail.com>
2009-09-11 03:14:43 -0600

    So far it looks like JMS is mostly working, can't receive though.

---
+++ src/main/java/org/randd/jms/TestHornetSendReceive.java
@@ -0,0 +1,140 @@
+package org.randd.jms;
+
+import org.hornetq.jms.HornetQQueue;
+import org.jboss.seam.ScopeType;
+import org.jboss.seam.annotations.Create;
+import org.jboss.seam.annotations.Destroy;
+import org.jboss.seam.annotations.Logger;
+import org.jboss.seam.annotations.Name;
+import org.jboss.seam.annotations.Scope;
+import org.jboss.seam.log.Log;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.Date;
+
+@Name("hornetSendReceive")
+@Scope(ScopeType.SESSION)
+public class TestHornetSendReceive
+{
+   private Queue testQueue;
+   private ConnectionFactory cf;
+   private Connection conn;
+   private boolean messageSent;
+   private boolean messageReceived;
+
+   @Logger
+   private Log log;
+
+   @Create
+   public void init()
+   {
+       InitialContext ic = null;
+       try
+       {
+           ic = new InitialContext();
+           this.cf = (ConnectionFactory) ic.lookup("java:comp/env/jms/connectionFactory");
+           this.testQueue = (Queue) ic.lookup("java:comp/env/jms/queues/testQueue");
+
+           this.conn = this.cf.createConnection();
+           this.conn.start();
+
+           this.log.debug("result of jms/testQueue lookup: {0}", this.testQueue);
+           this.log.debug("jms/testQueue address: {0}", ((HornetQQueue) this.testQueue).getAddress());
+       }
+       catch (Exception e)
+       {
+           this.log.error("Error retrieving from JNDI", e);
+       }
+       finally
+       {
+           try
+           {
+               ic.close();
+           }
+           catch (NamingException e)
+           {
+               // Eat exception
+           }
+       }
+   }
+
+   public void sendMessage()
+   {
+       try
+       {
+           Session s = this.conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+           s.createQueue("jms.queue.testQueue");
+           MessageProducer mp = s.createProducer(this.testQueue);
+
+           TextMessage txtMsg = s.createTextMessage("Hello from Hornet at: " + new Date());
+           mp.send(txtMsg);
+
+           s.close();
+           this.messageSent = true;
+       }
+       catch (JMSException e)
+       {
+           this.log.error("Error sending message", e);
+           this.messageSent = false;
+       }
+   }
+
+   public void receiveMessage()
+   {
+       try
+       {
+           Session s = this.conn.createSession(false, Session.AUTO_ACKNOWLEDGE);
+           MessageConsumer mc = s.createConsumer(this.testQueue);
+
+           TextMessage receivedMsg = (TextMessage) mc.receive(1000);
+           this.messageReceived = true;
+       }
+       catch (JMSException e)
+       {
+           this.log.error("Error receiving the message", e);
+           this.messageReceived = false;
+       }
+   }
+
+   @Destroy
+   public void cleanup()
+   {
+       try
+       {
+           this.conn.stop();
+       }
+       catch (JMSException e)
+       {
+           this.log.error("Could not stop JMS connection", e);
+       }
+   }
+
+    public boolean isMessageSent()
+    {
+        return messageSent;
+    }
+
+    public void setMessageSent(boolean messageSent)
+    {
+        this.messageSent = messageSent;
+    }
+
+    public boolean isMessageReceived()
+    {
+        return messageReceived;
+    }
+
+    public void setMessageReceived(boolean messageReceived)
+    {
+        this.messageReceived = messageReceived;
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -5,6 +5,7 @@
   <body>
     <ul>
       <li><a href="./welcome.seam">Weclome Page</a> -- Simply a configuration test</li>
+        <li><a href="./testHornetJMS.seam">Testing Hornet JMS</a> -- Simply a configuration test</li>
     </ul>
   </body>
 </html>
